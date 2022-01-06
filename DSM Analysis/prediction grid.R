54bb13e158b118fb978886cae1a78d73cd9dc8e8
Christopher-98 <cjm42@st-andrews.ac.uk>
2022-01-06 14:04:14 +0000

    initial commit, containing prediction grid reqired for dsm. can be explicitly set in file.

---
+++ DSM Analysis/prediction grid.R
@@ -0,0 +1,13 @@
+predgrid <- st_make_grid(st_bbox(region@region),
+                          cellsize = c(design.trunc, design.trunc))
+
+predgrid <- st_intersection(predgrid, region@region)
+
+predgrid <- st_as_sf(predgrid)
+
+predgrid$area <- as.numeric(st_area(predgrid))
+
+predgrid <- st_centroid(predgrid)
+
+preddata <- cbind(as.data.frame(st_drop_geometry(predgrid)),
+                  st_coordinates(predgrid))
