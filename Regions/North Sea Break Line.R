aeb6983ade07cf1f79e1848951c37d4a1863185b
Christopher-98 <cjm42@st-andrews.ac.uk>
2022-02-12 21:22:14 +0000

    create new region with small areas of high density

---
+++ Regions/North Sea Break Line.R
@@ -0,0 +1,71 @@
+
+library(dsims)
+
+# region to be north sea shape file
+region <- make.region(region.name = "North Sea Break",
+                      shape = "Regions/RegionPrj.shp")
+
+cover <- make.coverage(region = region,
+                       n.grid.points = 1000)
+design.trunc = 5
+
+design <- make.design(region = region,
+                             transect.type = 'line',
+                             design.angle = 90,
+                             samplers = 25,
+                             truncation = design.trunc,
+                             coverage.grid = cover)
+
+
+samplers <- generate.transects(design, region)
+
+plot(region, samplers)
+
+
+density <- make.density(region = region,
+                        x.space = 5,
+                        constant = 1)
+
+density <- add.hotspot(density, c(250, 400), 10, 1.5)
+density <- add.hotspot(density, c(300, 300), 10, 2.5)
+density <- add.hotspot(density, c(100, 700), 30, -0.5)
+density <- add.hotspot(density, c(-50, 700), 10, 1.5)
+density <- add.hotspot(density, c(100, 800), 20, 3)
+density <- add.hotspot(density, c(50, 400), 10, 2)
+density <- add.hotspot(density, c(150, 400), 10, 2)
+density <- add.hotspot(density, c(0, 500), 10, 2)
+density <- add.hotspot(density, c(-50, 600), 10, 2)
+density <- add.hotspot(density, c(-50, 900), 10, 2)
+density <- add.hotspot(density, c(100, 500), 10, 2)
+density <- add.hotspot(density, c(0, 800), 10, 2)
+density <- add.hotspot(density, c(170, 550), 10, 2)
+density <- add.hotspot(density, c(70, 620), 10, 2)
+
+
+plot(density, region)
+
+pop.desc <- make.population.description(region = region,
+                                        density = density,
+                                        N=1000)
+
+detect <- make.detectability(key.function = "hn",
+                             scale.param = 2.5,
+                             truncation = design.trunc)
+
+plot(detect, pop.desc)
+
+analyses <- make.ds.analysis(dfmodel = list(~1),
+                             key = "hn",
+                             er.var = "S1",
+                             truncation = design.trunc)
+
+sim <- make.simulation(reps = 1,
+                       design = design,
+                       population.description = pop.desc,
+                       detectability = detect,
+                       ds.analysis = analyses)
+
+
+survey <- run.survey(sim)
+
+plot(survey)
