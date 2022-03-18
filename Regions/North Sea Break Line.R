1806a8d6095515f3c02fd1408e03260c7795e913
Christopher-98 <cjm42@st-andrews.ac.uk>
2022-03-18 16:21:55 +0000

    adjust sampler numbers and modifiy density surface to be low around the edges of the study region

--- Regions/North Sea Break Line.R
+++ Regions/North Sea Break Line.R
@@ -7,12 +7,12 @@ region <- make.region(region.name = "North Sea Break",
 
 cover <- make.coverage(region = region,
                        n.grid.points = 1000)
-design.trunc = 5
+design.trunc = 10
 
 design <- make.design(region = region,
                              transect.type = 'line',
                              design.angle = 90,
-                             samplers = 25,
+                             samplers = 20,
                              truncation = design.trunc,
                              coverage.grid = cover)
 
@@ -24,31 +24,17 @@ plot(region, samplers)
 
 density <- make.density(region = region,
                         x.space = 5,
-                        constant = 1)
+                        constant = 0.25)
 
-density <- add.hotspot(density, c(250, 400), 10, 1.5)
-density <- add.hotspot(density, c(300, 300), 10, 2.5)
-density <- add.hotspot(density, c(100, 700), 30, -1)
-density <- add.hotspot(density, c(-50, 700), 10, 1.5)
-density <- add.hotspot(density, c(100, 850), 20, 3)
-density <- add.hotspot(density, c(50, 400), 10, 2)
-density <- add.hotspot(density, c(150, 400), 10, 2)
-density <- add.hotspot(density, c(0, 500), 10, 2)
-density <- add.hotspot(density, c(-50, 600), 10, 2)
-density <- add.hotspot(density, c(-50, 900), 10, 2)
-density <- add.hotspot(density, c(100, 500), 10, 2)
-density <- add.hotspot(density, c(0, 800), 10, 2)
-density <- add.hotspot(density, c(170, 550), 10, 2)
-density <- add.hotspot(density, c(70, 620), 10, 2)
-
-# Extract density values 
-densities <- get.densities(density)
-# Shift the values down so minimum density values are 0 (no scaling required)
-min.d <- min(densities)
-densities <- densities-min.d
-# Put adjusted densities back in density grid
-density <- set.densities(density, densities)
-
+density <- add.hotspot(density, c(150, 450), 50, 2)
+#density <- add.hotspot(density, c(250, 350), 50, -1)
+#density <- add.hotspot(density, c(100, 700), 30, -1)
+density <- add.hotspot(density, c(-50, 700), 30, 2)
+density <- add.hotspot(density, c(100, 850), 20, 2.5)
+#density <- add.hotspot(density, c(0, 450), 70, -0.5)
+density <- add.hotspot(density, c(0, 825), 50, 2.5)
+density <- add.hotspot(density, c(70, 620), 30, 2.5)
+#density <- add.hotspot(density, c(-100, 700), 75, -1)
 
 plot(density, region)
 
@@ -57,7 +43,7 @@ pop.desc <- make.population.description(region = region,
                                         N=1000)
 
 detect <- make.detectability(key.function = "hn",
-                             scale.param = 2.5,
+                             scale.param = 5,
                              truncation = design.trunc)
 
 plot(detect, pop.desc)
