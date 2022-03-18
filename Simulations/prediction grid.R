39555954405f91dad5053a2a2e4e083b1abb5586
Christopher-98 <cjm42@st-andrews.ac.uk>
2022-03-18 16:22:52 +0000

    add comments

--- Simulations/prediction grid.R
+++ Simulations/prediction grid.R
@@ -1,16 +1,19 @@
+
+# create grid across the area of the region
 predgrid <- st_make_grid(st_bbox(region@region),
                           cellsize = c(design.trunc*0.5, design.trunc*0.5))
 
+# crop grid to outline of region to allow ds comparison 
 predgrid <- st_intersection(predgrid, region@region)
 
-
+# convert to sf and calculate area of each grid segment
 predgrid <- st_as_sf(predgrid)
-
 predgrid$area <- as.numeric(st_area(predgrid))
 
+# convert so spatial and find centres of each grid segment
 pred.poly <- as(predgrid, 'Spatial')
-
 predgrid <- st_centroid(predgrid)
 
+# combine data as required by dsm
 preddata <- cbind(as.data.frame(st_drop_geometry(predgrid)),
                   st_coordinates(predgrid))
